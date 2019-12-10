45e2322f85077d6d3d24a2f6dc672de1d78720e7
encoreLinux <sheepoo1@naver.com>
2019-12-10 17:43:51 +0900

    add front

---
+++ client/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import Title from './components/Title'
+
+function App() {
+  return (
+    <Title/>
+  );
+}
+
+export default App;
---
+++ client/src/components/Title.jsx
@@ -0,0 +1,14 @@
+import React, {component} from 'react';
+
+class Title extends React.Component{
+
+    render(){
+        return(
+            <div>
+                <h1>타이틀</h1>
+            </div>
+        )
+    }
+}
+
+export default Title
